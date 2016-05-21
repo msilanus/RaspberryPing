a60518969938642455b83b4d6240124669c86438
msilanus <msilanus@gmail.com>
2016-05-21 13:17:24 +0200

    Introduce TIMEOUT in Cping.cpp

--- Cping.cpp
+++ Cping.cpp
@@ -13,7 +13,10 @@ Cping::~Cping()
 
 int Cping::getDistance()
 {
-	clock_t t1,t2;
+	// TIMOUT = 20ms
+	#define TIMEOUT 0.02
+	
+	clock_t t0,t1,t2;
 	// Attendre avant la prochaine mesure
 	Cgpio::ecrire(0);
 	usleep(500);
@@ -22,18 +25,23 @@ int Cping::getDistance()
 	usleep(10);
 	Cgpio::ecrire(0);
 	
-	// Attendre le passage a 1 de l'echo			
-	while(Cgpio::lire()==0) ;
+	// Attendre le passage a 1 de l'echo
+	t0=clock();		
+	while(Cgpio::lire()==0)
+	{
+		// Pas de passage à 1 avant le timeout
+		if((clock() - t0)/CLOCKS_PER_SEC < TIMEOUT) return -1;
+	}
 	t1=clock();
 		
 	// Attendre le retour à 0 de l'echo
-	while(Cgpio::lire()==1) ;
+	while(Cgpio::lire()==1 && (clock() - t1)/CLOCKS_PER_SEC < TIMEOUT) ;
 	t2=clock();
 	
-	// Calclu de la duree aller-retour
-	unsigned long duration = (((unsigned long)t2)-((unsigned long)t1));
-	// Calcul de la distance
-	float distance=duration*34/2000;
+	// Calcul de la duree aller-retour (CLOCKS_PER_SEC : nb de tick/s)
+    unsigned long duration = (((unsigned long)t2)-((unsigned long)t1))/CLOCKS_PER_SEC;
+    // Calcul de la distance (340 m/s trajet aller-retour)
+    float distance=duration*34000/2;
 	
 	return distance;
 }
